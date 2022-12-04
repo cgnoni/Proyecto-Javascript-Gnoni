2a0799ab991ab3b32d1ac6204a59314231ce79c3
cgnoni <c.gnoni1@gmail.com>
2022-12-04 17:14:32 -0300

    Agregando archivos al repositorio

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Variables 
+
+let nombre = "Dani";
+let edad = 40;
+
+if (edad <= 18 || nombre == "Dani") {
+    console.log("Bienvenido al casino!");
+} else {
+    console.log("No puede entrar al casino!");
+}
