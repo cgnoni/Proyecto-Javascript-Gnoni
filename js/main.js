3ed11299fb80ba4cab8886e43e9e96a80767a21d
cgnoni <c.gnoni1@gmail.com>
2022-12-05 00:01:55 -0300

    Creando simulador de construcción de triángulos según sus lados

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-//Variables 
+// Construcción de triángulos
 
-let nombre = "Dani";
-let edad = 40;
+// Variables globales
+let inicio = alert("¡Comencemos a jugar! Da click en Aceptar :)");
+let ladoA = prompt("Ingrese la medida (en cm) del lado A");
+let ladoB = prompt("Ingrese la medida (en cm) del lado B");
+let ladoC = prompt("Ingrese la medida (en cm) del lado C");
+let respuesta = 0;
 
-if (edad <= 18 || nombre == "Dani") {
-    console.log("Bienvenido al casino!");
-} else {
-    console.log("No puede entrar al casino!");
+// Funciones
+function construccionTriangulo() {
+  if (ladoA < ladoB + ladoC && ladoB < ladoA + ladoC && ladoC < ladoB + ladoA) {
+    console.log("¡Felicidades! Has construido un triángulo.");
+  } else if (ladoA == 0 || ladoB == 0 || ladoC == 0) {
+    console.log("Los lados no pueden ser iguales a 0. Intentalo nuevamente");
+  } else {
+    console.log("¡Oh no! Vuelve a intentarlo!");
+  }
+}
+
+construccionTriangulo();
+while ((respuesta = prompt("¿Desea seguir?\nEscriba 'Si' o 'No'"))) {
+  switch (respuesta) {
+    case "Si":
+      alert("Presione F5 para comenzar de nuevo!");
+      break;
+    case "No":
+      alert("¡Vuelve pronto!");
+      break;
+    default:
+      alert("Error.");
+  }
+  return;
 }
